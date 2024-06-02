eae0fa5378ec22b9a1f1cab61a561cefb4fd6cf3
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-02 20:31:27 +0200

    creating the post view and urls

--- posts/models.py
+++ posts/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=True)  
     image = models.ImageField(
         upload_to='images/', default='../user_1144760_1_vut7ya', blank=True
     )
