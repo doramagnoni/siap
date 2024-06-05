0fd7b2adfbe82b23dc6d2e4f963cf9d85cfaefd4
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-05 21:52:18 +0200

    implementing filters to the stories views

--- stories/views.py
+++ stories/views.py
@@ -1,13 +1,32 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Story
 from .serializers import StorySerializer
 
 class StoryList(generics.ListCreateAPIView):
     queryset = Story.objects.all()
     serializer_class = StorySerializer
-    permission_classes = [permissions.IsAdminUser] 
+    permission_classes = [permissions.IsAdminUser]
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'author', 'category', 'status'
+    ]
+    search_fields = [
+        'title', 'content'
+    ]
+    ordering_fields = [
+        'created_at',
+    ]
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
 class StoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Story.objects.all()
     serializer_class = StorySerializer
-    permission_classes = [permissions.IsAdminUser] 
+    permission_classes = [permissions.IsAdminUser]
