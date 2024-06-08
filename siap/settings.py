bffd52ed285ff6ebc7ac6085566e2ba266a6b9b6
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-08 07:23:54 +0200

    adding a coma to separate the strings in cors alloweed origins

--- siap/settings.py
+++ siap/settings.py
@@ -56,7 +56,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000" 
+    "http://127.0.0.1:3000",
     "https://siap-9ff06f350cf8.herokuapp.com" 
 ]
 
